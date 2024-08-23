d01d3a9c3784a17ae2a45f372deb6a7848d5c2dd
Hanspreet Singh <hshanjra.dev@gmail.com>
2024-08-23 12:56:42 +0530

    setup initial project

---
+++ src/constants/index.ts
@@ -0,0 +1,23 @@
+export const SITE_METADATA = {
+  name: "Buyurparts.com",
+  title: "Buy parts for your vehicle - buyurparts.com",
+  description:
+    "Buy auto parts and accessories for your Vehicle | Purchase across hundreds of Brands | Buy Ur Parts",
+  url: "https://buyurparts.com",
+  image: "https://buyurparts.com/logo.png",
+  twitter: "@buyurparts",
+  facebook: "buyurparts",
+  instagram: "buyurparts",
+  linkedin: "buyurparts",
+  email: "info@buyurparts.com",
+  supportEmail: "support@buyurparts.com",
+  phone: "+1 1234 5678 90",
+  address: {
+    line1: "1230 Maecenas Street Road",
+    line2: "",
+    city: "New York",
+    state: "NY",
+    zipCode: "10001",
+    country: "United States",
+  },
+};
