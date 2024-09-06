737309c7a857d4c097b51e948311bce55f4514cf
Hanspreet Singh <hshanjra.dev@gmail.com>
2024-09-06 18:30:31 +0530

    update NotFoundPage.tsx

--- src/components/Errors/NotFoundPage.tsx
+++ src/components/Errors/NotFoundPage.tsx
@@ -11,9 +11,9 @@ function NotFoundPage() {
           <div className="text-center my-10 max-w-md">
             <h2 className="text-3xl font-bold">UH OH! You're lost.</h2>
             <p className="mt-5">
-              The page you are looking for does not exist. How you got here is a
-              mystery. But you can click the button below to go back to the
-              homepage.
+              The page you are looking for does not exist.
+              <br /> How you got here is a mystery. But you can click the button
+              below to go back to the homepage.
             </p>
             <Button className="mt-5 rounded-full" asChild>
               <a href="/">Go Home</a>
